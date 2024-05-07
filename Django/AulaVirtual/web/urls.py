d189c1fc90841376004d1a708dbd684ccc3f16fb
Luciano Pueyo <l.pueyo@bue.edu.ar>
2024-05-07 20:18:17 -0300

    Clase 7: Views 1 y 2

--- Django/AulaVirtual/web/urls.py
+++ Django/AulaVirtual/web/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('saludar/<str:nombre>', views.saludar, name='saludar'),
+    re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio'),
 ]
--- Django/AulaVirtual/web/views.py
+++ Django/AulaVirtual/web/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola Mundo Django")
+    return render(request, 'index.html')
+
+def saludar(request, nombre):
+    print(request.method)
+
+    return HttpResponse(f"<h1>Bienvenid@ {nombre}</h1>")
+
+def alumnos_por_año(request, year):
+    alumnos = ["Carlos", "Maria", "Jose"] # """"Levanta""""" los usuarios de la BBDD
+    return HttpResponse(f"listado de alumnos: {year} \n {alumnos}")
