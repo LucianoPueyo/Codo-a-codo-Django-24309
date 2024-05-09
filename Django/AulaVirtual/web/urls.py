7a9de0a57cd0a8bddb4aea41fd7bcf0db5eb7b57
Luciano Pueyo <l.pueyo@bue.edu.ar>
2024-05-09 20:44:21 -0300

    Clase 8: Templates

--- Django/AulaVirtual/web/urls.py
+++ Django/AulaVirtual/web/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('saludar/<str:nombre>', views.saludar, name='saludar'),
     re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio'),
+    path('listado_alumnos', views.listado_alumnos, name='lista_alumnos'),
 ]
--- Django/AulaVirtual/web/views.py
+++ Django/AulaVirtual/web/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def index(request):
-    return render(request, 'index.html')
+    # Accedo a la BBDD a traves de los modelos
+
+    contexto = {
+        'name': 'Maria',
+        'fecha_hora': datetime.datetime.now()
+    }
+
+    return render(request, 'web/index.html', contexto)
 
 def saludar(request, nombre):
     print(request.method)
@@ -12,3 +20,17 @@ def saludar(request, nombre):
 def alumnos_por_año(request, year):
     alumnos = ["Carlos", "Maria", "Jose"] # """"Levanta""""" los usuarios de la BBDD
     return HttpResponse(f"listado de alumnos: {year} \n {alumnos}")
+
+def listado_alumnos(request):
+
+    contexto = {
+        'alumnos': [
+            'Carlos Lopez',
+            'Maria Del Cerro',
+            'Gaston Perez'
+        ],
+
+        'cuota_al_dia': True
+    }
+
+    return render(request, 'web/listado_alumnos.html', contexto)
