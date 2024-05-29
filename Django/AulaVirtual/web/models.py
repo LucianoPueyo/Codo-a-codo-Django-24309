ec2bbb3c764ca51f1e23449166abdfa618873787
Luciano Pueyo <l.pueyo@bue.edu.ar>
2024-05-28 21:20:44 -0300

    Clase 13: Presentaciones parciales 2 y modelos 1

--- Django/AulaVirtual/web/models.py
+++ Django/AulaVirtual/web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100, verbose_name="Nombre")
+    apellido = models.CharField(max_length=100, verbose_name="Apellido")
+    dni = models.IntegerField(verbose_name="DNI", unique=True)
+
+
+# Crear Migracion
+# python manage.py makemigrations web
+
+# Ejecutar migracion
